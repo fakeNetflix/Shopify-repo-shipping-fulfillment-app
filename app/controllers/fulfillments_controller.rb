48629c58aeef2e5abd274ffb2a2d6ca284cc54f5
David Thomas <david.thomas@shopify.com>
2012-07-05 11:52:23 -0400

    fixed off-center flash messages

--- app/controllers/fulfillments_controller.rb
+++ app/controllers/fulfillments_controller.rb
@@ -1,11 +1,11 @@
 class FulfillmentsController < ApplicationController
   def create
     if (params.has_key? :orders) && (Fulfillment.fulfill(session[:shop], params[:orders], params[:shipping_method], params[:tracking_number]))
-      flash[:success] = "Order fulfillment request successfully sent."
+      flash[:notice] = "Order fulfillment request successfully sent."
     elsif (params.has_key? :items) && (Fulfillment.fulfill(session[:shop], [params[:id]], params[:shipping_method], params[:tracking_number], params[:items]))     
-      flash[:success] = "Line Item fulfillment request successfully sent."      
+      flash[:notice] = "Line Item fulfillment request successfully sent."      
     else
-      flash[:errors] = "Invalid request."
+      flash[:alert] = "Invalid request."
     end
     redirect_to orders_url
   end
