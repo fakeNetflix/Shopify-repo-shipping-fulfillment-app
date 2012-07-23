4d6eb3d85791a84c8d8a462954d2ddac3ff0be9e
David Thomas <david.thomas@shopify.com>
2012-07-23 10:03:13 -0400

    rename workers folder to jobs

--- app/workers/variant_synchronizer.rb
+++
@@ -1,8 +0,0 @@
-class VariantSynchronizer
-  @que = :variant_synchronizer_que
-
-  def self.perform
-    # TODO: can we fetch all stock levels at once?
-    Variant.find_each {|variant| variant.fetch_stock_levels}
-  end
-end
